f4764bffeb5a25d48a132bd1e506ec83dbe5bc92
Nikhil Paranjape <nikhil1.paranjape@gmail.com>
2014-09-29 17:33:12 -0400

    new project, old one was causing issues

---
+++ myParksandRec/myParksandRec/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myParksandRec
+//
+//  Created by Nikhil Paranjape on 9/29/14.
+//  Copyright (c) 2014 NikhilP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
